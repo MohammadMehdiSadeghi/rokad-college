3003fe3ca0cce17fbd1880c1478b8e1f59fc46d6
mohammad mahdi sadeghi <mohammad12345sadeghi@gmail.com>
2026-09-08 14:55:32 +0330

    edit college

--- src/App.jsx
+++ src/App.jsx
@@ -122,6 +122,7 @@ export default function App() {
                 <Faq />
                 <Comments />
                 <Blogs />
+                <Blogs />
               </>
             )}
           </main>
